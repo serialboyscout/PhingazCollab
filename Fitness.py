a3cddafd0a5db30805d2bc7b12aac3ccfefc6889
JJB-123 <BROWNJASONJAY@GMAIL.COM>
2014-09-28 17:17:27 -0500

    Just updating with a Comment!

--- Fitness.py
+++ Fitness.py
@@ -2,15 +2,6 @@
 ''' Just a starting .py file for the github repository. Needed a file present to create a repository from Eclipse
     and commit to github.'''
     
+''' Looks good G!! JJB-123 '''
     
-
-
-
-
-
-
-
-
-
-
 
