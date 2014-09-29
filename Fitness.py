58ed956b8757db3afcafcbac8bdb5876f4aff0cb
JJB-123 <BROWNJASONJAY@GMAIL.COM>
2014-09-28 23:09:52 -0500

    Added an IF statement to determine the size of the Random Int

--- Fitness.py
+++ Fitness.py
@@ -8,6 +8,11 @@ import random
     
 intTest = random.randint(0,100)
    
+if intTest >= 50:
+    print 'The random # is bigger than 50'
+else:
+    print 'The random # is smaller than 50'
+
 print(intTest)
 
 print 'Check out this integer: %d' % intTest
