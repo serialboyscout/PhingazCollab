e998c7b38613b98f2f2da779b6e091b58595e6fa
JJB-123 <BROWNJASONJAY@GMAIL.COM>
2014-09-28 23:35:27 -0500

    Built a function for Odd or Even - Also added comments for clarity

--- Fitness.py
+++ Fitness.py
@@ -4,14 +4,33 @@
     
 ''' Looks good G!! JJB-123 '''
 
+
+
+''' Odd or Even function
+Uses the Modulus operator to determine Odd or Even '''
+def OddorEven(n):
+    if n % 2 == 0:
+        print 'The integer is Even!'
+        return 
+    else:
+        print 'The integer is Odd!'
+        return 
+
+
+'''MAIN START'''
+    
 import random
     
 intTest = random.randint(0,100)
-   
+
+# This IF statement just looks to see if the random is bigger or smaller than 50  
 if intTest >= 50:
     print 'The random # is bigger than 50'
 else:
     print 'The random # is smaller than 50'
+
+# This function returns an Odd or Even statement 
+OddorEven(intTest)
 
 print(intTest)
 
